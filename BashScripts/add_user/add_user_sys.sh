b1722df6a0435f2dcdcbb1ef3a426aad59b5295c
Edgar Kaziahmedov <edos@virtuozzo.mipt.ru>
2016-10-05 18:08:45 +0300

    New programs and fixes

--- BashScripts/add_user/add_user_sys.sh
+++ BashScripts/add_user/add_user_sys.sh
@@ -5,7 +5,7 @@ USER_GROUPS=""
 HOME_DIRECTORY=""
 HASH_PASSWD=""
 PASSWD=""
-MAX_USERS=65536
+WATCHDOG=65536
 
 CUR_USER=`id -u`
 if [ "$CUR_USER" -ne 0 ];
@@ -14,16 +14,41 @@ then
         exit 1;
 fi
 
-for i in seq 1 "$MAX_USERS"
+for i in seq 1 "$WATCHDOG"
 do
-        echo "Enter user name:";
-        read -r USER_NAME;
+        #Enter user name
+        while [[ -z "${USER_NAME// }" ]]
+        do
+                echo "Enter user name:";
+                read -r USER_NAME;
+        done
+        #Enter list of groups
         echo "Enter preferred groups(separated by a comma, without whitespaces):";
         read -r USER_GROUPS;
-        echo "Enter your home directory(if it doesn't exist, it will be created):";
-        read -r HOME_DIRECTORY; 
-        echo "Enter password hash:"
-        read -r HASH_PASSWD;
+        #Enter home directory
+        while [[ -z "${HOME_DIRECTORY// }" ]] 
+        do
+                echo "Enter your home directory(if it doesn't exist, it will be created):";
+                read -r HOME_DIRECTORY; 
+        done
+        #Enter hash is created by crypt(sha-512 alg with sault)
+        while [[ -z "${HASH_PASSWD// }" ]]
+        do
+                echo "Enter password hash:"
+                read -r HASH_PASSWD;
+        done
+
         useradd -d "$HOME_DIRECTORY" -G "$USER_GROUPS" -m -p "$HASH_PASSWD" "$USER_NAME"
+
+        if [ $? -ne 0 ];
+        then
+                echo "useradd failed";
+                exit $?;
+        fi
+
+        USER_NAME="";
+        USER_GROUPS="";
+        HOME_DIRECTORY="";
+        HASH_PASSWD="";
 done
 
