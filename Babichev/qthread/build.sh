4dacaf18fa369c01ce45baa75b3f18842d50491f
Edgar Kaziakhmedov <edgar.kaziakhmedov@virtuozzo.com>
2017-12-24 17:18:46 +0300

    qthread: Improve build.sh
    Signed-off-by: Edgar Kaziakhmedov <edgar.kaziakhmedov@virtuozzo.com>

--- Babichev/qthread/build.sh
+++ Babichev/qthread/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 moc -o main.moc main.cpp
-g++ -o main ./main.cpp -DQT_SHARED -I/usr/include/qt4 -I/usr/include/qt4/QtCore -lQtCore
+g++ -o main ./main.cpp `pkg-config --libs --cflags QtCore`
