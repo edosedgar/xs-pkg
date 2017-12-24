ebd5b45c8ea3a485ff0a15172b4fcbfd7a8ca1b4
Edgar Kaziakhmedov <edgar.kaziakhmedov@virtuozzo.com>
2017-12-24 17:12:33 +0300

    qthread: stable version
    There was a race condition, so fix it and make polished.
    
    Signed-off-by: Edgar Kaziakhmedov <edgar.kaziakhmedov@virtuozzo.com>

--- Babichev/qthread/main.cpp
+++ Babichev/qthread/main.cpp
@@ -3,55 +3,34 @@
 #include <sstream>
 #include <unistd.h>
 
-/*class Worker : public QObject
-{
-    Q_OBJECT
-
-public:
-    void pass_id(int id, int id2)
-    {
-        q_id = id;
-        l_id = id2;
-    }
-
-private:
-    int q_id;
-    int l_id;
-signals:
-    void wake_up();
-private slots:
-};
-*/
 class Thread : public QThread
 {
     Q_OBJECT
 
 public:
-    void pass_id(int id, int id2)
+    Thread() : id(0) {};
+    void set_id(int id)
     {
-        q_id = id;
-        l_id = id2;
+        this->id = id;
+    }
+    void do_work() {
+        qDebug() << "Internal id: "<< id;
+        emit notify_next();
+        quit();
     }
 
 private:
-    int q_id;
-    int l_id;
+    int id;
 
     void run()
     {
-        if (q_id == 0) {
-            emit notify_next();
-            return;
-        }
         exec();
     }
 signals:
     void notify_next();
 public slots:
-    void do_work() {
-        qDebug() << "Internal id: "<< q_id;
-        emit notify_next();
-        quit();
+    void do_work_handler() {
+            do_work();
     }
 };
 
@@ -60,6 +39,8 @@ public slots:
 int main(int argc, char *argv[])
 {
     QCoreApplication loop(argc, argv);
+    int thread_num = 0;
+    int i;
 
     if (argc != 2) {
         std::cerr << "Usage: ./main THREAD_NUM\n";
@@ -68,27 +49,24 @@ int main(int argc, char *argv[])
 
     std::stringstream conv(argv[1]);
 
-    int thread_num = 0;
     conv >> thread_num;
 
     Thread *thread = new Thread[thread_num];
-    int i;
 
-    qDebug() << "Internal id: "<< 0;
-    for (i = 0; i < thread_num; i++) {
-        thread[i].pass_id(i, thread_num);
-        if (i != thread_num - 1) {
-            QObject::connect(&thread[i], SIGNAL(notify_next()),
-                             &thread[i+1], SLOT(do_work()));
-            thread[i+1].moveToThread(&thread[i]);
-        }
+    for (i = 0; i < thread_num - 1; i++) {
+        thread[i].set_id(i);
+        QObject::connect(&thread[i], SIGNAL(notify_next()),
+                         &thread[i + 1], SLOT(do_work_handler()));
     }
-    sleep(1);
+    thread[thread_num - 1].set_id(thread_num - 1);
+
     for (i = 0; i < thread_num; i++)
         thread[i].start();
-    for (i = 0; i < thread_num; i++) {
+
+    thread[0].do_work();
+
+    for (i = 0; i < thread_num; i++)
         thread[i].wait();
-    }
 
     delete [] thread;
     return 0;
